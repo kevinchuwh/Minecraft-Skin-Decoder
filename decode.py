1ea33a3dc17ebae8387164ecda499fa5fafd8492
XPS <kevinchuwinghim@icloud.com>
2016-06-17 22:03:49 +0800

    Update decode.py

--- decode.py
+++ decode.py
@@ -4,17 +4,17 @@ import json
 
 def decodeB64(code):
     bas64text = base64.b64decode(code)
-    baseUTF = bas64text.decode("utf-8")
-    crop = baseUTF.split("\"url\":\"")[1]
+    baseUTF = bas64text.decode('utf-8')
+    crop = baseUTF.split('\"url\":\"')[1]
     url = crop[:-4]
     print(url)
 
 def getSkin(UUID):
-    page = urllib.request.urlopen("https://sessionserver.mojang.com/session/minecraft/profile/" + UUID)
-    content = page.read().decode("utf-8")
+    page = urllib.request.urlopen('https://sessionserver.mojang.com/session/minecraft/profile/' + UUID)
+    content = page.read().decode('utf-8')
     json_obj = json.loads(content)
     list_d = json_obj['properties']
     return list_d[0]['value']
 
     
-decodeB64(getSkin(input("Enter UUID > ")))
+decodeB64(getSkin(input('Enter UUID > ')))
